ea5aef2f40fc25a77e4966ec5d68570a529989b8
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-08-07 05:35:27 +0500

    Portfolio Section done

--- project-6/src/pages/Dashboard/components/PortfolioSection.jsx
+++ project-6/src/pages/Dashboard/components/PortfolioSection.jsx
@@ -1,42 +1,56 @@
-import { HStack, Stack, Text, Icon, Tag } from '@chakra-ui/react'
+import { HStack, Stack, Text, Icon, Tag,Button } from '@chakra-ui/react'
 import { AiOutlineInfoCircle } from "react-icons/ai"
+import { LuArrowDownToLine } from "react-icons/lu";
+import { LuArrowUpToLine } from "react-icons/lu";
 
 function PortfolioSection() {
     return (
-        <HStack p="24px" bg="white" borderRadius="xl" spacing={16}>
-            <Stack>
-                <HStack color="black.80" >
-                    <Text fontSize="sm"  >
-                        Total Portfolio Value
-                    </Text>
-                    <Icon as={AiOutlineInfoCircle} />
-                </HStack>
-                <Text textStyle="h2">
-                    $ 109,897.20
-                </Text>
-            </Stack>
-            <Stack >
-                <HStack color="black.80" >
-                    <Text fontSize="sm"  >
-                        Wallet Balances
-                    </Text>
-                </HStack>
-                <HStack spacing={8}>
-                    <HStack>
+        <>
+            <HStack p="24px" bg="white" borderRadius="xl" spacing={16} justify="space-between" >
+                <HStack spacing={10}>
+                    <Stack>
+                        <HStack color="black.80" >
+                            <Text fontSize="sm"  >
+                                Total Portfolio Value
+                            </Text>
+                            <Icon as={AiOutlineInfoCircle} />
+                        </HStack>
                         <Text textStyle="h2">
-                            22.39401000
+                            $ 109,897.20
                         </Text>
-                        <Tag>BTC</Tag>
+                    </Stack>
+                    <Stack >
+                        <HStack color="black.80" >
+                            <Text fontSize="sm"  >
+                                Wallet Balances
+                            </Text>
+                        </HStack>
+                        <HStack spacing={5}>
+                            <HStack>
+                                <Text textStyle="h2">
+                                    22.39401000
+                                </Text>
+                                <Tag colorScheme="gray">BTC</Tag>
+                            </HStack>
+                            <HStack>
+                                <Text textStyle="h2">
+                                    $ 1200.31
+                                </Text>
+                                <Tag colorScheme="gray">USD</Tag>
+                            </HStack>
+                        </HStack>
+                    </Stack>
+                </HStack>
+                <HStack>
+                    <HStack>
+                        <Button leftIcon={<Icon as={LuArrowDownToLine} />}>   Deposit</Button>
                     </HStack>
                     <HStack>
-                        <Text textStyle="h2">
-                           $ 1200.31
-                        </Text>
-                        <Tag>USD</Tag>
+                        <Button leftIcon={<Icon as={LuArrowUpToLine} />}>Withdraw</Button>
                     </HStack>
                 </HStack>
-            </Stack>
-        </HStack>
+            </HStack> {/* Portfolio Section */}
+        </>
     )
 }
 
