7b36167a4f3449bbe5cd8d466c7b3085400d968c
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-08-09 19:16:05 +0500

    Sign Up form handling and validation done with formik and yup

--- project-7/client/src/pages/Auth/SignUp.jsx
+++ project-7/client/src/pages/Auth/SignUp.jsx
@@ -1,57 +1,159 @@
-import React from 'react'
-import { Card,Container,Center,Text,Stack, FormControl,FormLabel,Input,Flex, Checkbox,Button } from "@chakra-ui/react"
-import {Link} from "react-router-dom"
+import React, { useState } from 'react';
+import {
+  Card, Container, Center, Text, Stack,
+  FormControl, FormLabel, Input, Flex, Checkbox, Button, FormErrorMessage
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("First name is required"),
+  lastname: Yup.string()
+    .required("Last name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  repeatpassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], "Passwords must match")
+    .required("Repeat password is required"),
+});
 
 function SignUp() {
-    return (
-        <Container>
-            <Center minH={"100vh"}>
-                <Card borderRadius={"16px"} p={6} w="456px"  >
-                    <Text textStyle={"h1"}>Welcome to Crypto</Text>
-                    <Text textStyle={"p2"} color={"black.60"} mt={"4"}>Create a free account by filling data below</Text>
-                    <Stack mt={"10"} spacing={"6"}>
-                        <Flex gap={"6"} flexDir={{ base: "column", sm: "row" }} >
-                            <FormControl>
-                                <FormLabel htmlFor="name">Name</FormLabel>
-                                <Input name="name" placeholder="Enter Your name"  />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor="lastname">Last Name</FormLabel>
-                                <Input name="lastname" placeholder="Enter Your Last Name" />
-                            </FormControl>
-                        </Flex>
-                        <FormControl>
-                                <FormLabel htmlFor="email">Email</FormLabel>
-                                <Input name="email" type='email' placeholder="Enter Your Email" />
-                        </FormControl>
-                        <FormControl>
-                                <FormLabel htmlFor="password">Password</FormLabel>
-                                <Input name="password" type='password' placeholder="Enter Your Password" />
-                        </FormControl>
-                         <FormControl>
-                                <FormLabel htmlFor="repeat-password">Repeat Password</FormLabel>
-                                <Input name="repeat-password" type='password' placeholder="Enter Your Repeat Password" />
-                        </FormControl>
-                        <Checkbox mt={"4"}>
-                            <Text textStyle={"p3"}>
-                                I agree with
-                                <Text as="span" color={"p.purple"}> Terms & Conditions</Text>
-                            </Text>
-                        </Checkbox>
-                        <Button>Create an account</Button>
-                        <Flex justify={"center"}>
-                            <Text textStyle={"p3"}>Already have an account?
-                            <Link to="/sign-in"> 
-                                <Text as="span" color={"p.purple"}> Sign In</Text>
-                            </Link>
-                        </Text>
-                        </Flex>
-                    </Stack>
-                </Card>
-            </Center>
-        </Container>
-    )
+  const {
+    handleBlur, handleChange, handleSubmit, values, touched, errors } = useFormik({
+    initialValues: {
+      name: '',
+      lastname: '',
+      email: '',
+      password: '',
+      repeatpassword: '',
+    },
+    validationSchema,
+    onSubmit: (values,{resetForm}) => {
+      console.log("Form submitted:", values);
+      setformData(values);
+      resetForm();
+      
+    },
+  });
+
+  const [formData,setformData]=useState({});
+  
+
+  return (
+    <Container>
+      <Center minH={"100vh"}>
+        <Card borderRadius={"16px"} p={6} w="456px">
+          <Text textStyle={"h1"}>Welcome to CryptoApp</Text>
+          <Text textStyle={"p2"} color={"black.60"} mt={"4"}>
+            Create a free account by filling data below
+          </Text>
+
+          <form onSubmit={handleSubmit}>
+            <Stack mt={"10"} spacing={"6"}>
+              {/* Name + Lastname */}
+              <Flex gap={"6"} flexDir={{ base: "column", sm: "row" }}>
+                <FormControl isInvalid={touched.name && !!errors.name}>
+                  <FormLabel htmlFor="name">Name</FormLabel>
+                  <Input
+                    name="name"
+                    placeholder="Enter Your Name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                  />
+                  <FormErrorMessage>{errors.name}</FormErrorMessage>
+                </FormControl>
+
+                <FormControl isInvalid={touched.lastname && !!errors.lastname}>
+                  <FormLabel htmlFor="lastname">Last Name</FormLabel>
+                  <Input
+                    name="lastname"
+                    placeholder="Enter Your Last Name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.lastname}
+                  />
+                  <FormErrorMessage>{errors.lastname}</FormErrorMessage>
+                </FormControl>
+              </Flex>
+
+              {/* Email */}
+              <FormControl isInvalid={touched.email && !!errors.email}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  name="email"
+                  type='email'
+                  placeholder="Enter Your Email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <FormErrorMessage>{errors.email}</FormErrorMessage>
+              </FormControl>
+
+              {/* Password */}
+              <FormControl isInvalid={touched.password && !!errors.password}>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  name="password"
+                  type='password'
+                  placeholder="Enter Your Password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <FormErrorMessage>{errors.password}</FormErrorMessage>
+              </FormControl>
+
+              {/* Repeat Password */}
+              <FormControl isInvalid={touched.repeatpassword && !!errors.repeatpassword}>
+                <FormLabel htmlFor="repeatpassword">Repeat Password</FormLabel>
+                <Input
+                  name="repeatpassword"
+                  type='password'
+                  placeholder="Repeat Your Password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.repeatpassword}
+                />
+                <FormErrorMessage>{errors.repeatpassword}</FormErrorMessage>
+              </FormControl>
+
+              {/* Checkbox */}
+              <Checkbox mt={"4"}>
+                <Text textStyle={"p3"}>
+                  I agree with
+                  <Text as="span" color={"p.purple"}> Terms & Conditions</Text>
+                </Text>
+              </Checkbox>
+
+              {/* Submit Button */}
+              <Button type="submit" colorScheme="purple">
+                Create an account
+              </Button>
+
+              {/* Link */}
+              <Flex justify={"center"}>
+                <Text textStyle={"p3"}>
+                  Already have an account?
+                  <Link to="/sign-in">
+                    <Text as="span" color={"p.purple"}> Sign In</Text>
+                  </Link>
+                </Text>
+              </Flex>
+            </Stack>
+          </form>
+        </Card>
+      </Center>
+    </Container>
+  );
 }
 
-export default SignUp
+export default SignUp;
